4b1c2a559a25375857f04ad69a41b568bc3d154e
emilezi <75627932+emilezi@users.noreply.github.com>
2024-06-24 15:20:25 +0200

    Add files via upload

--- functions/popup.js
+++ functions/popup.js
@@ -6,6 +6,14 @@
 
 /*
 *
+* Constant parameter language
+*
+*/
+
+const language = (navigator.language || navigator.userLanguage).substr(0, 2);
+
+/*
+*
 * Yes or No Dialog Box
 *
 */
@@ -13,8 +21,16 @@
 function PopUpRadio(action,value) {
 
     var elem = document.getElementById('popup_form_container');
+    
+    if(language === 'fr'){
 
-    elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Êtes-vous vraiment sûr de vouloir effectuer l'action demandée ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='" + action + value + "' value='Oui'></form><input type='submit' onclick=PopUpClose() value='Non'></div></div></div>";
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Êtes-vous vraiment sûr de vouloir effectuer l'action demandée ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='" + action + value + "' value='Oui'></form><input type='submit' onclick=PopUpClose() value='Non'></div></div></div>";
+
+    }else{
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><p>Are you sure you want to perform the requested action ?</p><div class='pop-up-element-form'><form method='post'><input type='submit' name='" + action + value + "' value='Yes'></form><input type='submit' onclick=PopUpClose() value='No'></div></div></div>";
+        
+    }
 
 }
 
@@ -27,8 +43,16 @@ function PopUpRadio(action,value) {
 function PopUpInterfaceNameEdit(value) {
 
     var elem = document.getElementById('popup_form_container');
+    
+    if(language === 'fr'){
 
-    elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Modification du nom de l'interface</h5><form method='post'><input name='post_interface_name' type='text' placeholder='Saisissez un nom' value='"+value+"' required/><br/><br/><input type='submit' name='submit_interface_name_edit' value='Modifier le nom'/></form></div></div>";
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Modification du nom de l'interface</h5><form method='post'><input name='post_interface_name' type='text' placeholder='Saisissez un nom' value='"+value+"' required/><br/><br/><input type='submit' name='submit_interface_name_edit' value='Modifier le nom'/></form></div></div>";
+
+    }else{
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Changing the interface name</h5><form method='post'><input name='post_interface_name' type='text' placeholder='Enter a name' value='"+value+"' required/><br/><br/><input type='submit' name='submit_interface_name_edit' value='Change name'/></form></div></div>";
+        
+    }
 
 }
 
@@ -42,7 +66,15 @@ function PopUpInterfaceBackgroundEdit() {
 
     var elem = document.getElementById('popup_form_container');
 
-    elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Modification de l'image de fond</h5><form enctype='multipart/form-data' method='post'><input type='hidden' name='MAX_FILE_SIZE' value='2500000' /><input type='file' name='background'/><input type='submit' name='submit_background_edit' value='Modifier la photo de fond'/></fom></div></div>";
+    if(language === 'fr'){
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Modification de l'image de fond</h5><form enctype='multipart/form-data' method='post'><input type='hidden' name='MAX_FILE_SIZE' value='2500000' /><input type='file' name='background'/><input type='submit' name='submit_background_edit' value='Modifier la photo de fond'/></fom></div></div>";
+
+    }else{
+
+        elem.innerHTML = "<div class='pop-up-container-form animation-pop-up-form'><div class='pop-up-form'><h5>Changing the background image</h5><form enctype='multipart/form-data' method='post'><input type='hidden' name='MAX_FILE_SIZE' value='2500000' /><input type='file' name='background'/><input type='submit' name='submit_background_edit' value='Change background photo'/></fom></div></div>";
+        
+    }
 
 }
 
