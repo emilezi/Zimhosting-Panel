7885124dec7fa3dda1e62da279fc40adb16f55ac
emilezi <75627932+emilezi@users.noreply.github.com>
2026-02-20 20:51:23 +0100

    Add files via upload

--- public/page/fr/admin/about.php
+++ public/page/fr/admin/about.php
@@ -16,13 +16,13 @@ echo "<p>La plupart des services étant open sources, certains sont protégés p
 
 echo "<p>Vous avez donc la possibilité de retrouver les sources des développeurs concernés grâce au lien si dessous</p>";
 
-echo "<p>Sources : <a href='https://emilezimmer.fr/index.php?page=zimhosting_panel_apps'>https://emilezimmer.fr/index.php?page=zimhosting_panel_apps</a></p>";
+echo "<p>Sources : <a href='https://emile.ovh/index.php?page=zimhosting_panel_apps'>https://emile.ovh/index.php?page=zimhosting_panel_apps</a></p>";
 
 echo "<p>Attention : Il est de votre responsabilité de respecter la législation en vigueur concernant la protection de chacun et la réglementation en vigueur concernant internet.</p>";
 
 echo "<p>Réalisé par Emile Z.<p>";
 
-echo "<p>Site officiel : <a href='https://emilezimmer.fr/'>emilezimmer.fr</a></p>";
+echo "<p>Site officiel : <a href='https://emile.ovh/'>emile.ovh</a></p>";
 
 echo "<p>Version : 1.2</p>";
 
